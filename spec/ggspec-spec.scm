91d4aa34605eabc4292c8588a0d3dbb88581afdb
Yawar Amin <yawar.amin@gmail.com>
2014-02-08 22:01:50 -0500

    Fix up copyright, license message

--- spec/ggspec-spec.scm
+++ spec/ggspec-spec.scm
@@ -1,8 +1,26 @@
-;; ggspec - lightweight unit testing library for Guile
-;; Copyright (c) 2014 Yawar Amin
-;; See LICENSE file for details
-;; GitHub, Reddit, Twitter: yawaramin
+#!
+ggspec-spec - unit tests that verify the behaviour of ggspec.
 
+Copyright (c) 2014 Yawar Amin
+GitHub, Reddit, Twitter: yawaramin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+!#
 (suite "The kwalist function"
   (tests
     (test "Should return an empty list if it is given an empty list"
